368704c667f4d91751820bcb7a38a61d0dd62514
osbahzad <osb@andrew.cmu.edu>
2024-10-28 17:15:36 -0400

    Initial Commit

---
+++ StutterQuest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StutterQuest
+//
+//  Created by Omar Bahzad on 10/28/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StutterQuest/StutterQuestApp.swift
@@ -0,0 +1,17 @@
+//
+//  StutterQuestApp.swift
+//  StutterQuest
+//
+//  Created by Omar Bahzad on 10/28/24.
+//
+
+import SwiftUI
+
+@main
+struct StutterQuestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
