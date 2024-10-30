66a3e5ac27d145c9488740ca58b2bb83e4a2e14e
Veronica Benedict <vbenedic@andrew.cmu.edu>
2024-10-30 18:09:30 -0400

    Added tabs for signing up or signing in to toggle

---
+++ StutterQuest/Views/Components/CustomToggle.swift
@@ -0,0 +1,18 @@
+//
+//  CustomToggle.swift
+//  StutterQuest
+//
+//  Created by Veronica Benedict on 10/30/24.
+//
+
+import SwiftUI
+
+struct CustomToggle: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomToggle()
+}
--- StutterQuest/Views/ContentView.swift
+++ StutterQuest/Views/ContentView.swift
@@ -8,12 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
+  @State private var signingUp = true
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+          CustomToggle(signingUp: $signingUp)
         }
         .padding()
     }
