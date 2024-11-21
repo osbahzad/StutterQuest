64ca8b79a3da49e57845117426015432c11893bf
osbahzad <osb@andrew.cmu.edu>
2024-11-20 20:42:58 -0500

    Tutorial

---
+++ StutterQuest/Views/TutorialView.swift
@@ -0,0 +1,18 @@
+//
+//  TutorialView.swift
+//  StutterQuest
+//
+//  Created by Omar Bahzad on 11/20/24.
+//
+
+import SwiftUI
+
+struct TutorialView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TutorialView()
+}
